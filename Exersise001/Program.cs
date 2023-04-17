a3afd92487a464d727d6f4ffc3ba69640361ec22
Alexey_Zotov <Alexey.Zotov@ymail.com>
2023-04-17 13:53:53 +0300

    exersise001 project added

---
+++ Exersise001/Program.cs
@@ -0,0 +1,18 @@
+﻿bool IsPalindrome(int number)
+{
+    char[] stringArray = number.ToString().ToCharArray();
+    Array.Reverse(stringArray);
+    int reversedNumber = Convert.ToInt32(new string(stringArray));
+
+    return reversedNumber == number;
+}
+
+Console.Clear();
+Console.WriteLine("Program to check if number is palindrome");
+Console.Write("Enter your value: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (IsPalindrome(number))
+    Console.WriteLine("\nValue is Palindrome\n");
+else
+    Console.WriteLine("Value not a Palindrome");
