3f96a6d1ab790090331eca7081f1a82366310038
Alexey_Zotov <Alexey.Zotov@ymail.com>
2023-04-17 14:27:47 +0300

    Exersise002 porject added

---
+++ Exersise002/Program.cs
@@ -0,0 +1,27 @@
+﻿double GetDistance(int[] pointA, int[] pointB)
+{
+    return Math.Round(Math.Sqrt(Math.Pow((pointB[0] - pointA[0]), 2) +
+        Math.Pow((pointB[1] - pointA[1]), 2) +
+        Math.Pow((pointB[2] - pointA[2]), 2)), 2);
+}
+
+int[] GetPoint()
+{
+    int arrayLength = 3;
+    int[] array = new int[arrayLength];
+    for (int i = 0; i < arrayLength; i++)
+    {
+        Console.Write($"Enter coordinate {i + 1}: ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+
+    return array;
+}
+
+Console.Clear();
+Console.WriteLine("Program to calculate distance betwen two 3D points");
+Console.WriteLine($"Enter first point");
+int[] pointA = GetPoint();
+Console.WriteLine($"Enter second point");
+int[] pointB = GetPoint();
+Console.WriteLine($"Distance between 2 points is: {GetDistance(pointA, pointB)}");
