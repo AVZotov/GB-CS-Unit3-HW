039093925c99e6883c0b22f312ecf1c23466cda7
Alexey_Zotov <Alexey.Zotov@ymail.com>
2023-04-17 14:36:44 +0300

    Exersise003 project added

---
+++ Exersise003/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.WriteLine("Program to print 3rd degree of values from 1 to N");
+Console.Write("Enter your value: ");
+int userValue = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= userValue; i++)
+{
+    Console.Write($"{Math.Pow(i, 3)} ");
+}
